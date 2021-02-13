6bfc4f18c82d542171020fc231eb758825781bdb
claudiofei <78588569+claudiofei@users.noreply.github.com>
2021-02-13 12:46:48 +0100

    Creazione aggiunta file spring

---
+++ src/main/java/it/sirfin/autojpa/AutojpaApplication.java
@@ -0,0 +1,13 @@
+package it.sirfin.autojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutojpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutojpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/sirfin/autojpa/AutojpaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sirfin.autojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutojpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
